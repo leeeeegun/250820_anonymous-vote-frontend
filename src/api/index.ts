b99e6cd68ef8cff9a07e8219c8fd66353ffa9233
leeeeegun <leeeeegun@naver.com>
2025-08-20 14:23:53 +0900

    feat: [이의건] API 세팅 [AVF-FRONT-01]

---
+++ src/api/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8888/api',
+});
+
+// 질문 조회
+export const getQuestions = async () => {
+    const res = await api.get('/questions');
+    return res.data;
+};
+
+// 질문 생성
+export const createQuestion = async (title: string) => {
+    const res = await api.post('/questions', title);
+    return res.data;
+};
+
+// 옵션 추가
+export const addOption = async (questionId: number, content: string) => {
+    const res = await api.post(`/questions/${questionId}/options`, { content });
+    return res.data;
+};
+
+// 투표하기
+export const voteOption = async (optionId: number) => {
+    const res = await api.post(`/questions/${optionId}/vote`);
+    return res.data;
+};
+
+// 결과 조회
+export const getResults = async (questionId: number) => {
+    const res = await api.get(`/questions/${questionId}/results`);
+    return res.data;
+};
